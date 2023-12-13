ff0b27d9e0130210cbec4ebbe83b0538342edda5
Bekir Kurubas <bekirkurubas6767@gmail.com>
2023-12-13 11:01:59 +0100

    data folder has added

---
+++ data/dataClasses.js
@@ -0,0 +1,20 @@
+export let classes =[
+    {
+      id: 1,
+      name: "Full Stack Class",
+      teacher: "Maria Smith",
+      description: "Fullstack Web Programming Expert!",
+    },
+    {
+      id: 2,
+      name: "AWS Cloud Class",
+      teacher: "Ali Sayar",
+      description: "AWS Cloud Expert!",
+    },
+    {
+      id: 3,
+      name: "Software Testing Class",
+      teacher: "Hannas Bühner",
+      description: "Javascript Expert!",
+    },
+     ];
---
+++ data/dataTeachers.js
@@ -0,0 +1,21 @@
+export let teachers=[
+    { id: 1,
+      name: "Maria Smith",      
+      description: "Fullstack Web Programming Expert!",
+      classId:1,
+      className:"Full Stack Class"      
+    },
+    { id: 2,
+      name: "Ali Sayar",      
+      description: "AWS Cloud Expert!",
+      classId:2,
+      className:"AWS Cloud Class"     
+    },
+    { id: 3,
+      name: "Hannas Bühner",      
+      description: "Javascript Expert!",
+      classId:2,
+      className:"Software Testing Class"    
+      
+    },
+];
